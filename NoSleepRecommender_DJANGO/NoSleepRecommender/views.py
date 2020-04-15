a8337811aa899f3aae0ee0511756bd3ded9a6de8
Kanishkah Anwari <kanwari3@gatech.edu>
2020-04-14 21:07:03 -0400

    wordcloud

--- NoSleepRecommender_DJANGO/NoSleepRecommender/views.py
+++ NoSleepRecommender_DJANGO/NoSleepRecommender/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from NoSleepRecommender.controllers.RedditAPI import RedditAPI
 from NoSleepRecommender.controllers.DynamoDBAPI import DynamoDBAPI
+from NoSleepRecommender.plotly_wordcloud import plotly_wordcloud
+
 
 def home(request):
     args = {}
     text = ""
     args['mytext'] = text
-    #rd = RedditAPI()
-    #stories = rd.get_stories()
     db = DynamoDBAPI()
-    #print(stories)
     args['recent_stories'] = db.get_stories()
     return render(request, 'index.html', args)
 
@@ -24,12 +23,15 @@ def results(request):
     if request.method == 'POST':
         text = request.POST.get('search_key')
         stories = db.get_recommendations(text)
-        #print(stories)
-
+    text = ''
     args['results'] = text
     args['current_story'] = stories['current_story']
     args['recommendations'] = stories['recommendations']
     args['total_recommendations'] = len(stories['recommendations'])
+    for story in stories['recommendations'].body:
+        text += story
+    args['wordcloud'] = plotly_wordcloud(text)
+
     return render(request, 'results.html', args)
 
 
